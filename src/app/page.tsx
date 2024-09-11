e3d8c7f6be549d71485bf38afb196aa852dfaa0c
Mégane Menanteau <megane.menanteau@gmail.com>
2024-09-11 18:09:52 +0200

    Add daisyUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ const Home = () => {
   const books = fetchBooks();
 
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
       <main className="container mx-auto p-8">
         <SearchBar onChange={(value: string) => setSearchValue(value)} />
         <span>{searchValue}</span>
         <ul className="grid grid-cols-3 gap-6 mt-8">
           {books.map((book) => (
-            <li key={book.id} className="flex bg-neutral-700">
+            <li key={book.id} className="card card-side bg-base-100 p-4">
               <Image
                 className="w-auto h-auto"
                 src={book.cover}
@@ -25,8 +25,10 @@ const Home = () => {
                 width={100}
                 height={200}
               />
-              <div className="text-white">
-                {book.title} - {book.author.firstname} {book.author.lastname}
+              <div className="card-body">
+                <div className="card-title text-white">
+                  {book.title} - {book.author.firstname} {book.author.lastname}
+                </div>
               </div>
             </li>
           ))}
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,6 +7,7 @@ interface SearchBarProps {
 const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
   return (
     <input
+      className="input input-bordered input-primary w-full max-w-xs text-white"
       type="search"
       placeholder="Search here"
       onChange={(event) => props.onChange(event.target.value)}
